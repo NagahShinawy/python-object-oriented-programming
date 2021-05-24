53075d5fa20fc0298bdb0c0723488953bf07ccf0
Nagah Shinawy <E-n.Shinawy@lean.sa>
2021-05-24 11:29:11 +0300

    classmethod, staticmethod

--- ch1/1-definition_start.py
+++ ch1/1-definition_start.py
@@ -5,10 +5,10 @@ created by Nagaj at 23/05/2021
 # Python Object Oriented Programming by Joe Marini course example
 # Basic class definitions
 books = [
-    {"title": "clean code", "price": 30.12, "author": "bob", "pages": 300},
-    {"title": "Brave New World", "price": 50.24, "author": "james", "pages": 500},
-    {"title": "War and Peace", "price": 10.99, "author": "john", "pages": 100},
-    {"title": "django for web apps", "price": 15.99, "author": "open source", "pages": 1200},
+    {"title": "clean code", "price": 30.12, "author": "bob", "pages": 300, "booktype": "PAPERBACK"},
+    {"title": "Brave New World", "price": 50.24, "author": "james", "pages": 500, "booktype": "HARDCOVER"},
+    {"title": "War and Peace", "price": 10.99, "author": "john", "pages": 100, "booktype": "EBOOK"},
+    {"title": "django for web apps", "price": 15.99, "author": "open source", "pages": 1200, "booktype": "EBOOK"},
 
 ]
 
@@ -17,11 +17,51 @@ books = [
 
 
 class Book:
+    # TODO: Properties defined at the class level are shared by all instances
+    BOOK_TYPES = ("HARDCOVER", "PAPERBACK", "EBOOK")
+
+    # TODO: double-underscore properties are hidden from other classes
+    __booklist = None
+
+    # TODO: create a class method
+
+    @classmethod
+    def getbooktypes(cls):
+        return cls.BOOK_TYPES
+
+    # TODO: create a static method
+
+    @staticmethod
+    def getbooklist():
+        if Book.__booklist is None:
+            Book.__booklist = []
+        return Book.__booklist
+
+    def save(self):
+        booklist = Book.getbooklist()
+        if self not in booklist:
+            booklist.append(self)
+            print(f"<{self}> was saved!")
+        else:
+            print(f"<{self}> already saved!")
+
+    def remove(self):
+        booklist = Book.getbooklist()
+        if self in booklist:
+            booklist.remove(self)
+            print(f"<{self}> was removed!")
+
+    @classmethod
+    def whoami(cls):
+        print(f"I'm Class <{cls.__name__}>")
+
+    def whoobj(self):
+        print(f"I'm Obj <{self}> of Class <{self.__class__.__name__}>")
 
     # the "init" function is called when the instance is
     # created and ready to be initialized
 
-    def __init__(self, title, price, author, pages):
+    def __init__(self, title, price, author, pages, booktype):
         # instance attr: unique for each instance(obj)
         self.title = title
         self.price = price
@@ -29,6 +69,10 @@ class Book:
         self.pages = pages
         self._discount = 0
         self.__private = "private"
+        if booktype not in Book.BOOK_TYPES:
+            raise ValueError(f"<{booktype}> is not valid booktype, {Book.BOOK_TYPES}")
+        else:
+            self.booktype = booktype
 
     def __str__(self):
         return f"{self.title} By {self.author}"
@@ -49,7 +93,7 @@ class Book:
         print(f"Pages: {self.pages}")
 
 
-class Ebook(Book):
+class EBook(Book):
 
     def change_private(self):
         # print(self.__private)  # error name mangling
@@ -104,8 +148,8 @@ def main():
     # but still you can access it, private in python is just convention
     print(b1._Book__private)  # Name mangling
     b1._Book__private = 10000000
-    print(b1._Book__private)   # changed
-    eb = Ebook(b4.title, b4.price, b4.author, b4.pages)
+    print(b1._Book__private)  # changed
+    eb = EBook(b4.title, b4.price, b4.author, b4.pages, b4.booktype)
     eb.change_private()
 
     # TODO: use type() to inspect the object type
@@ -122,23 +166,51 @@ def main():
 
     print(type(eb))  # <class '__main__.Ebook'>
 
-    print(isinstance(eb, Ebook))   # True  Ebook is subclass
+    print(isinstance(eb, EBook))  # True  Ebook is subclass
 
     print(isinstance(eb, Book))  # True Book is superclass
 
     # TODO: compare two types together
 
-    print(type(b1) == type(b2))   # True
+    print(type(b1) == type(b2))  # True
     print(type(b1) == type(times))  # False
     print("#" * 50)
     # TODO: use isinstance to compare a specific instance to a known type
 
-    print(isinstance(b2, Book))   # True
-    print(isinstance(b2, Ebook))  # False
+    print(isinstance(b2, Book))  # True
+    print(isinstance(b2, EBook))  # False
     print(isinstance(b2, Newspaper))  # True
     print(isinstance(b2, object))  # True
 
-    print(b2.__class__.__mro__)   # (<class '__main__.Book'>, <class 'object'>)
+    print(b2.__class__.__mro__)  # (<class '__main__.Book'>, <class 'object'>)
+
+    ################################################################################################
+    print("#" * 100)
+    # TODO: access the class attribute
+    print(Book.BOOK_TYPES)  # using class name
+    print(b1.BOOK_TYPES)  # using obj but not recommended, it is better to use class name
+
+    ################################################################################################
+    print("#" * 100)
+    # TODO: Use the static method to access a singleton object
+    allbooks = Book.getbooklist()  # use class name to access staticmethod (recommended)
+    # allbooks = b1.getbooklist()  # use obj to access static method
+    for book in [b1, b2, b3, b4]:
+        allbooks.append(book)
+
+    print(allbooks)
+    ################################################################################################
+    print("#" * 100)
+    Book.whoami()  # class method that its implementation depends on class not obj
+    EBook.whoami()
+    ################################################################################################
+    print("#" * 100)
+    b1.whoobj()
+    eb.whoobj()
+    ################################################################################################
+    print("#" * 100)
+    b1.remove()
+    b1.save()
 
 
 if __name__ == "__main__":
